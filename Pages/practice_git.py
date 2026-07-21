e0a1cc09808cddabcbd53904467c6b42b280f4f9
Solleti <rsolleti@luxoft.com>
2026-07-21 20:23:11 +0530

    working with branches

--- Pages/practice_git.py
+++ Pages/practice_git.py
@@ -1,1 +1,2 @@
 "Hello , i want to learn git and its branching concepts and want to understand conflicts in git and trying to learn conflicts in github"
+"now merging branch2 to main in the local and pushing main branch to main branch in remote repository"
